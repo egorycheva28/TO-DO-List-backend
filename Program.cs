1e587cce0c512daf10ef865f921ef2f48e2cfa4d
egorycheva28 <egor.masha28@list.ru>
2024-09-28 22:38:08 +0700

    добавила CORS для доступа к фронту

--- Program.cs
+++ Program.cs
@@ -10,21 +10,36 @@ builder.Services.AddDbContext <TodoContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
 );
 
+//добавляем CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+    builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseRouting();
 
-/*app.UseEndpoints(endpoints =>
+//включаем CORS
+app.UseCors("AllowAllOrigins");
+
+app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
-});*/
+});
 
 app.UseHttpsRedirection();
 
 //app.UseAuthorization();
-app.MapControllers();
+//app.MapControllers();
 //app.UseDeveloperExceptionPage();
 app.Run();
 
